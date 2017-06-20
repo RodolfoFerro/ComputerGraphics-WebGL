899dd35ea522f3ec66e7ab727d49caa686b54f90
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2017-06-19 21:33:40 -0500

    Updated cube_texture

--- cube_textured/app.js
+++ cube_textured/app.js
@@ -210,13 +210,13 @@ var InitWebGL = function (){
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-  if (document.getElementById('option1').checked){
+  if (document.getElementById('option1').checked == true){
     gl.texImage2D(
       gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
       gl.UNSIGNED_BYTE,
       document.getElementById('texture1')
     );
-  } else if (document.getElementById('option2').checked){
+  } else if (document.getElementById('option2').checked == true){
       gl.texImage2D(
         gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
         gl.UNSIGNED_BYTE,
