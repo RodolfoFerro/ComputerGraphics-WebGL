b3e5ad9ac91b59b18b237bf1b696026d766acdf9
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2017-06-19 21:27:07 -0500

    Updated cube_texture

--- cube_textured/app.js
+++ cube_textured/app.js
@@ -231,11 +231,6 @@ var InitWebGL = function (){
       );
     }
   }
-  gl.texImage2D(
-    gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
-    gl.UNSIGNED_BYTE,
-    document.getElementById(x)
-  );
   gl.bindTexture(gl.TEXTURE_2D, null);
 
   // Begin using program:
