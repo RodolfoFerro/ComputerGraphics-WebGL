21e53aeb834655b5fddbb6bffae2727007e72036
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2017-06-19 21:39:35 -0500

    Updated cube_texture

--- cube_textured/app.js
+++ cube_textured/app.js
@@ -30,7 +30,20 @@ var fragmentShaderText =
 '}'
 ].join('\n');
 
-var InitWebGL = function (){
+var selection = function (){
+  if (document.getElementById('option1').checked == true){
+    txt =  'texture1';
+    InitWebGL(txt);
+  } else if (document.getElementById('option2').checked == true){
+    txt = 'texture2';
+    InitWebGL(txt);
+  } else{
+    txt =  'texture3';
+    InitWebGL(txt);
+  }
+};
+
+var InitWebGL = function (texture){
 
   // Loading message:
   console.log("Successfuly loaded.");
@@ -210,28 +223,33 @@ var InitWebGL = function (){
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-  if (document.getElementById('option1').checked == true){
-    console.log("Texture 1 selected.");
-    gl.texImage2D(
-      gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
-      gl.UNSIGNED_BYTE,
-      document.getElementById('texture1')
-    );
-  } else if (document.getElementById('option2').checked == true){
-    console.log("Texture 2 selected.");
-    gl.texImage2D(
-      gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
-      gl.UNSIGNED_BYTE,
-      document.getElementById('texture2')
-    );
-  } else{
-    console.log("Texture 3 selected.");
-    gl.texImage2D(
-      gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
-      gl.UNSIGNED_BYTE,
-      document.getElementById('texture3')
-    );
-  }
+  // if (document.getElementById('option1').checked == true){
+  //   console.log("Texture 1 selected.");
+  //   gl.texImage2D(
+  //     gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
+  //     gl.UNSIGNED_BYTE,
+  //     document.getElementById('texture1')
+  //   );
+  // } else if (document.getElementById('option2').checked == true){
+  //   console.log("Texture 2 selected.");
+  //   gl.texImage2D(
+  //     gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
+  //     gl.UNSIGNED_BYTE,
+  //     document.getElementById('texture2')
+  //   );
+  // } else{
+  //   console.log("Texture 3 selected.");
+  //   gl.texImage2D(
+  //     gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
+  //     gl.UNSIGNED_BYTE,
+  //     document.getElementById('texture3')
+  //   );
+  // }
+  gl.texImage2D(
+    gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    document.getElementById(texture)
+  );
   gl.bindTexture(gl.TEXTURE_2D, null);
 
   // Begin using program:
